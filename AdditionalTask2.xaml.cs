cd6912292a0c6fbab231f962c7ed1631c7bee7a2
admin <admin@DESKTOP-FA5KDQG>
2024-06-03 13:53:51 +0300

    Add project files.

---
+++ AdditionalTask2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CounterTotalSum_WPF
+{
+    /// <summary>
+    /// Interaction logic for AdditionalTask2.xaml
+    /// </summary>
+    public partial class AdditionalTask2 : Window
+    {
+        private string[] colors = { "Navy", "Blue", "Aqua", "Teal", "Olive", "Green", "Lime", "Yellow", "Orange",
+        "Red", "Maroon", "Fuchsia", "Purple", "Black", "Silver", "Gray", "White" };
+        public AdditionalTask2()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Button button = new Button();
+                button.Content = colors[i];
+                button.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors[i]));
+                button.Margin = new Thickness(2.0);
+                wrapPanel.Children.Add(button);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CounterTotalSum_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private decimal totalPrice = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            totalPriceTextBox.Text = totalPrice.ToString();
+        }
+
+        private void countButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(decimal.TryParse(purchasePriceTextBox.Text, out decimal price))
+            {
+                totalPrice += price;
+                totalPriceTextBox.Text = totalPrice.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные данные!");
+            }
+        }
+    }
+}
